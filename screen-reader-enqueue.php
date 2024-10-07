4b01558d8aa0f067e2c79cb03bb329e6b84e3c2b
luis-from-kai <avaloseluis@gmail.com>
2024-10-07 19:19:44 -0400

    adding skeleton functions

--- screen-reader-enqueue.php
+++ screen-reader-enqueue.php
@@ -4,6 +4,15 @@
 <!-- we want to create a method that enqueues our custome JS filer
         for the screen reader -->
 
-function enqueue_script(){
+if(!defined('ABSPATH')){
+        exit;
+}
+
+
+<!-- funciton to enque scripts and stlyes -->
+
+function accesibility_enqueing_script() {
+
+        <!-- TODO: Register js, css files etc. -->
 
 }
