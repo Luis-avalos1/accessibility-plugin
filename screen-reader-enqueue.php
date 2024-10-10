0883cabc695208a464f95363b85824a4ab799f78
luis~ <avaloseluis@gmail.com>
2024-10-10 13:39:54 -0400

    adding correct php file structure

--- screen-reader-enqueue.php
+++ screen-reader-enqueue.php
@@ -1,18 +1,23 @@
 
 
+<?php 
 
-<!-- we want to create a method that enqueues our custome JS filer
-        for the screen reader -->
+// we want to create a method that enqueues our custome JS filer
+//         for the screen reader 
 
 if(!defined('ABSPATH')){
         exit;
 }
 
 
-<!-- funciton to enque scripts and stlyes -->
-
+//  funciton to enque scripts and stlyes
 function accesibility_enqueing_script() {
 
-        <!-- TODO: Register js, css files etc. -->
+// TODO: Register js, css files etc. \
+
+}
+
+
+function accesibility_enqueing_script_file() {
 
 }
