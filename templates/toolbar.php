16489cab448ef89bc52ecedc2ddd3132414de275
luis-from-kai <avaloseluis@gmail.com>
2024-10-12 00:31:31 -0400

    adding more to toolbar

--- templates/toolbar.php
+++ templates/toolbar.php
@@ -4,7 +4,40 @@
 <div id="toolbar" role ="navigation" aria-label="Accessibility Toolbar">
 
     {% comment %} TOGGLE OUR FEATURES {% endcomment %}
+
+    
+    {% comment %} Screen Reader Toggle {% endcomment %}
     <button id="screen-reader-toggle" class="accessibility-button" aria-pressed="false">
         <span class="accessibility-button-label">Screen Reader </span>
     </button>
 
+    {% comment %} TODO: fill in correct IDs! --> have to cross reference JS and CSS file {% endcomment %}
+    {% comment %} text features toggle {% endcomment %}
+    <button id="" class="accessibility-button" aria-label="Increase Text Size">
+        <span class="accessibility-button-label">Increase Text Size</span>
+    </button>
+    
+    <button id="" class="accessibility-button" aria-label="Decrease Text Size">
+        <span class="accessibility-button-label">Decrease Text Size</span>
+    </button>
+
+    {% comment %} Spacing  {% endcomment %}
+    <button id="" class="accessibility-button" aria-label="Increase Spacing">
+        <span class="accessibility-button-label">Increase Spacing</span>
+    </button>
+    
+    <button id="" class="accessibility-button" aria-label="Decrease Spacing">
+        <span class="accessibility-button-label">Decrease Spacing</span>
+    </button>
+    
+    {% comment %} Open Dyslexia Font {% endcomment %}
+    <button id="" class="accessibility-button" aria-pressed="false">
+        <span class="accessibility-button-label">Dyslexia Font</span>
+    </button>
+
+    
+    
+    
+
+    
+    
