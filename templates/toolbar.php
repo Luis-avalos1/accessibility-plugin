70638f854ac39d3d771c94144a69b87386a2ce2b
luis-from-kai <avaloseluis@gmail.com>
2024-10-11 20:47:32 -0400

    started developing toolbar

---
+++ templates/toolbar.php
@@ -0,0 +1,10 @@
+
+
+{% comment %} this will be a toolbar for our plug in  {% endcomment %}
+<div id="toolbar" role ="navigation" aria-label="Accessibility Toolbar">
+
+    {% comment %} TOGGLE OUR FEATURES {% endcomment %}
+    <button id="screen-reader-toggle" class="accessibility-button" aria-pressed="false">
+        <span class="accessibility-button-label">Screen Reader </span>
+    </button>
+
