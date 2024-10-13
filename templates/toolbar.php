fb7cd93c3aa598504d26114c2fd671cf4fe5159c
luis-from-kai <avaloseluis@gmail.com>
2024-10-13 19:50:08 -0400

    toolbar mostly finished, added colorblindness color modes to css

--- templates/toolbar.php
+++ templates/toolbar.php
@@ -1,41 +1,57 @@
 
 
-{% comment %} this will be a toolbar for our plug in  {% endcomment %}
+<!-- this will be a toolbar for our plug in -->
 <div id="toolbar" role ="navigation" aria-label="Accessibility Toolbar">
 
     {% comment %} TOGGLE OUR FEATURES {% endcomment %}
-
-    
     {% comment %} Screen Reader Toggle {% endcomment %}
     <button id="screen-reader-toggle" class="accessibility-button" aria-pressed="false">
         <span class="accessibility-button-label">Screen Reader </span>
     </button>
 
-    {% comment %} TODO: fill in correct IDs! --> have to cross reference JS and CSS file {% endcomment %}
+    
     {% comment %} text features toggle {% endcomment %}
-    <button id="" class="accessibility-button" aria-label="Increase Text Size">
+    <button id="increase-text-size" class="accessibility-button" aria-label="Increase Text Size">
         <span class="accessibility-button-label">Increase Text Size</span>
     </button>
     
-    <button id="" class="accessibility-button" aria-label="Decrease Text Size">
+    <button id="decrease-text-size" class="accessibility-button" aria-label="Decrease Text Size">
         <span class="accessibility-button-label">Decrease Text Size</span>
     </button>
 
     {% comment %} Spacing  {% endcomment %}
-    <button id="" class="accessibility-button" aria-label="Increase Spacing">
+    <button id="increase-spacing" class="accessibility-button" aria-label="Increase Spacing">
         <span class="accessibility-button-label">Increase Spacing</span>
     </button>
     
-    <button id="" class="accessibility-button" aria-label="Decrease Spacing">
+    <button id="decrease-spacing" class="accessibility-button" aria-label="Decrease Spacing">
         <span class="accessibility-button-label">Decrease Spacing</span>
     </button>
     
     {% comment %} Open Dyslexia Font {% endcomment %}
-    <button id="" class="accessibility-button" aria-pressed="false">
+    <button id="dyslexia-font-toggle" class="accessibility-button" aria-pressed="false">
         <span class="accessibility-button-label">Dyslexia Font</span>
     </button>
 
+    {% comment %} color mode {% endcomment %}
+    <select id="awe-color-mode-select" class="awe-select" aria-label="Select Color Mode">
+        <option value="default">Default</option>
+        <option value="protanopia">Protanopia</option>
+        <option value="deuteranopia">Deuteranopia</option>
+        <option value="tritanopia">Tritanopia</option>
+        <option value="high-contrast">High Contrast</option>
+        <option value="reset">Reset to Default</option>
+    </select>
     
+
+
+
+
+</div> <!-- end of master div --> 
+
+
+
+            
     
     
 
